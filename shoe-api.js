72928e3c895496e784cc2206a13e803915e14188
Your Name <Your Name>
2024-02-04 20:37:06 +0100

    init2

--- shoe-api.js
+++ shoe-api.js
@@ -33,7 +33,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static('../client'));
+app.use('/', express.static('./client'));
 
 app.post('/shoe', (req, res) => {
     const shoe = req.body;
